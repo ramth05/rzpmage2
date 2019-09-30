04620a4cb506f6c3958fba24bbaf60a36d48d21e
Ram Prakash Singh <ram.techchef@razorpay.com>
2019-09-30 18:25:19 +0530

    added phpunit

---
+++ Test/Unit/Model/PaymentActionTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Razorpay\Magento\Test\Unit\Model;
+use Magento\Payment\Model\Method\AbstractMethod;
+use Razorpay\Magento\Model\PaymentAction;
+class PaymentActionTest extends \PHPUnit\Framework\TestCase
+{
+    public function testToOptionArray()
+    {
+        $sourceModel = new PaymentAction();
+        static::assertEquals(
+            [
+                [
+                    'value' => AbstractMethod::ACTION_AUTHORIZE,
+                    'label' => __('Authorize Only')
+                ],
+                [
+                    'value' => AbstractMethod::ACTION_AUTHORIZE_CAPTURE,
+                    'label' => __('Authorize and Capture')
+                ]
+            ],
+            $sourceModel->toOptionArray()
+        );
+    }
+}
